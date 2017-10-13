d8f65844b1f37c014a3deac139f7da41458a9c30
gillyb <gillyb@gmail.com>
2017-10-13 19:29:49 +0300

    Close #23

--- public/js/components/Home.js
+++ public/js/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export default class HomeComponent extends React.Component {
 
@@ -7,12 +8,34 @@ export default class HomeComponent extends React.Component {
     }
 
     componentWillMount() {
-        // TODO: check if the user is logged in. if so, redirect to their resumes list
+        // TODO: check if the user is logged in. if so, redirect to their last edited resume
     }
 
     render() {
         return (
-            <div className="home">Hello world</div>
+            <div className="row justify-content-center">
+                <div className="col-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h4 className="card-title justify-content-center">Hello & Welcome</h4>
+                            <p className="card-text">Welcome to the easiest and funnest way to create a resume.</p>
+                            <p className="card-text">
+                                All you have to do to begin is click on the top menu
+                                <Link to="/resume">Create New Resume</Link>
+                                and a full resume will be created just for you. With details and all.<br/>
+                                Of course they won't be your details, but that can be changed very easily. All you have to do, is click
+                                on the different sections to edit or delete them to match your personal details.
+                            </p>
+                            <p className="card-text">
+                                You don't even need to be registered to start. Just start and have fun!<br/>
+                                Your resume will be saved locally, and if you'll want to keep it, or share it with others
+                                then you will need to sign in. But go ahead and try first before you commit.<br/>
+                                (Even though committing is free and really simple)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 
